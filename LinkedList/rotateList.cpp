d43c00aaa5b0d64340ca71286f566a657db2c31a
Anamika Gour <anamikagour666@gmail.com>
2024-01-13 23:01:31 +0530

    day-73

---
+++ LinkedList/rotateList.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void prinLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+}
+
+int getLength(Node *head)
+{
+    int i = 0;
+    while (head != NULL)
+    {
+        i++;
+        head = head->next;
+    }
+    return i;
+}
+Node *rotateRight(Node *head, int k)
+{
+    int len = getLength(head);
+    if (k == 0)
+        return head;
+    int actualRotateK = (k % len);
+    if (actualRotateK == 0)
+    {
+        return NULL;
+    }
+    int newLastNodePos = len - actualRotateK - 1;
+    Node *newLastNode = head;
+
+    for (int i = 0; i < newLastNodePos; i++)
+    {
+        newLastNode = newLastNode->next;
+    }
+    Node *newHead = newLastNode->next;
+    newLastNode->next = nullptr;
+
+    Node *it = newHead;
+    while (it->next)
+    {
+        it = it->next;
+    }
+    it->next = head;
+    return newHead;
+}
+
+int main()
+{
+    Node *head = new Node();
+    head->data = 12;
+
+    head->next = new Node();
+    head->next->data = 20;
+
+    head->next->next = new Node();
+    head->next->next->data = 25;
+
+    head->next->next->next = new Node();
+    head->next->next->next->data = 30;
+
+    cout << "Original LinkedList " << endl;
+    prinLinkedList(head);
+    cout << endl;
+    cout << "LinkedList after insertion" << endl;
+
+    prinLinkedList(head);
+    return 0;
+}
---
+++ LinkedList/sortLLcontaining012.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void prinLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+}
+Node *segregate(Node *head)
+{
+
+    // Add code here
+    Node *temp = head;
+    int one = 0, two = 0, zero = 0;
+    while (temp != NULL)
+    {
+        if (temp->data == 0)
+        {
+            zero++;
+        }
+        else if (temp->data == 1)
+        {
+            one++;
+        }
+        else
+        {
+            two++;
+        }
+        temp = temp->next;
+    }
+    temp = head;
+    while (zero--)
+    {
+        temp->data = 0;
+        temp = temp->next;
+    }
+    while (one--)
+    {
+        temp->data = 1;
+        temp = temp->next;
+    }
+    while (two--)
+    {
+        temp->data = 2;
+        temp = temp->next;
+    }
+    return head;
+}
+int main()
+{
+    Node *head = new Node();
+    head->data = 0;
+
+    head->next = new Node();
+    head->next->data = 1;
+
+    head->next->next = new Node();
+    head->next->next->data = 2;
+
+    head->next->next->next = new Node();
+    head->next->next->next->data = 0;
+
+    head->next->next->next->next = new Node();
+    head->next->next->next->next->data = 1;
+    head->next->next->next->next->next = new Node();
+    head->next->next->next->next->next->data = 2;
+
+    cout << "Original LinkedList " << endl;
+    prinLinkedList(head);
+    cout << endl;
+    cout << "LinkedList after sorting" << endl;
+    Node *newHead = segregate(head);
+    prinLinkedList(newHead);
+    return 0;
+}
